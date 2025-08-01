61d2c6c26799d70d589f4cf132a38952f3d5e7b6
Austinobravo <Austinobravo@gmail.com>
2025-08-01 18:41:08 +0100

    Did the buy a ticket screen

--- app/(buy-tickets)/buy-ticket/_components/BusTickets.tsx
+++ app/(buy-tickets)/buy-ticket/_components/BusTickets.tsx
@@ -16,10 +16,10 @@ const BusTickets = () => {
                 <h4 className='text-gray-500 text-lg font-medium'>Quickly find tickets based on where you're boarding and where you're going.</h4>
                 <BusTicketForm />
             </div>
-            <div className='flex flex-col justify-between'>
+            <div className='flex flex-col justify-between gap-4'>
                 <div className='bg-blue-50 p-4 rounded-2xl space-y-2'>
-                    <h3 className='text-gray-700 text-2xl font-medium'>All City Bus Tickets for Your Route</h3>
-                    <h4 className='text-gray-500 text-lg font-medium'>Browse available tickets based on your location and travel needs across Owerri.</h4>
+                    <h3 className='text-gray-700 text-lg lg:text-2xl font-medium'>All City Bus Tickets for Your Route</h3>
+                    <h4 className='text-gray-500 text-sm lg:text-lg font-medium'>Browse available tickets based on your location and travel needs across Owerri.</h4>
                      <Link href={`item.url`} className='text-blue-700 flex gap-3 pt-5 items-center font-medium'>
                         <span>View all Tickets</span>
                         <ChevronRight />
@@ -27,11 +27,11 @@ const BusTickets = () => {
                     </Link>
                 </div>
                 <div className='bg-[#F6FEF9] p-4 rounded-2xl space-y-2'>
-                    <h3 className='text-gray-700 text-2xl font-medium'>Recent Purchases</h3>
-                    <h4 className='text-gray-500 text-lg font-medium'>Quickly view and rebook tickets you've bought on City Bus.</h4>
+                    <h3 className='text-gray-700 text-lg lg:text-2xl font-medium'>Recent Purchases</h3>
+                    <h4 className='text-gray-500 text-sm lg:text-lg font-medium'>Quickly view and rebook tickets you've bought on City Bus.</h4>
                     <div className='flex flex-col items-center justify-center text-gray-500 py-5 gap-4'>
                         <CreditCard />
-                        <p>You have not purchased any tickets</p>
+                        <p className='text-sm text-center'>You have not purchased any tickets</p>
                     </div>
                 </div>
             </div>
