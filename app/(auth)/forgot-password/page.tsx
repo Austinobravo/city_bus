f4ce4cb14624de465e0ef8f33f611c1f2130d24a
Austinobravo <Austinobravo@gmail.com>
2025-08-24 19:29:52 +0100

    commit for push

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import ForgotPasswordForm from './_components/ForgotPasswordForm'
 
 const ForgotPasswordPage = () => {
   return (
-    <div className='flex flex-col justify-center items-center py-20 space-y-7'>
+    <div className='flex flex-col justify-center items-center px-7 py-14 space-y-10'>
         <h2 className='text-gray-700 text-3xl font-semibold'>Reset your password</h2>
         <ForgotPasswordForm />
     </div>
